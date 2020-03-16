cfaf38a0fcb30b00d74c6dfbfc72622621293e3b
Frédéric NOËL <contact@fredericnoel.com>
2020-03-16 09:47:51 +0100

    Inclusion classe Voiture Instanciation objet Voiture Dump

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
 date_default_timezone_set('Europe/Paris');
+
+// On va chercher la classe Voiture
+require_once './classes/Voiture.php';
+
+// Maintenant, je peux instancier un objet avec l'opérateur new
+$voiture1 = new Voiture();
+
+// Regardons comment se comporte notre objet
+var_dump($voiture1);
+
