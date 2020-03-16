05be51a751307d37b06fddede70231f7ee981a91
Frédéric NOËL <contact@fredericnoel.com>
2020-03-16 11:26:58 +0100

    La propriété masse par de public à private

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -5,7 +5,7 @@ class Voiture
     public string $marque; // Le typage des variables possible PHP 7.4
     public $modele;
     public $couleur;
-    public $masse;
+    private $masse;
     public $vitesse = 0;
 
     // On définit le constructeur
