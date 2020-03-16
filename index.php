0909a7aa0e03b0592edc21112d6c4012da0bd463
Frédéric NOËL <contact@fredericnoel.com>
2020-03-16 09:54:33 +0100

    Modifier des propriétés de l'objet

--- index.php
+++ index.php
@@ -8,6 +8,13 @@ require_once './classes/Voiture.php';
 // Maintenant, je peux instancier un objet avec l'opérateur new
 $voiture1 = new Voiture();
 
+$voiture1->marque = 'Lada';
+$voiture1->modele = 'Niva';
+$voiture1->couleur = 'moche';
+$voiture1->masse = 1000;
+
+
+
 // Regardons comment se comporte notre objet
 var_dump($voiture1);
 
