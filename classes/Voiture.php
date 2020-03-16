d547ecdca6883f28a1c06cc08ea7275ae6665cc2
Frédéric NOËL <contact@fredericnoel.com>
2020-03-16 10:19:14 +0100

    Constructeur et destructeur

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -7,4 +7,21 @@ class Voiture
     public $couleur;
     public $masse;
     public $vitesse = 0;
+
+    // On définit le constructeur
+    public function __construct(string $mar, string $mod, string $cou, int $mas)
+    {
+        // On affecte les paramètres passés à l'objet que l'on va instancier
+        // La variable $this désigne l'objet
+        $this->marque = $mar;
+        $this->modele = $mod;
+        $this->couleur = $cou;
+        $this->masse = $mas;
+    }
+
+        //Destructeur appelé automatiquement à la fin de l'exécution du script
+    public function __destruct()
+    {
+        echo 'Object destroyed !!!';
+    }
 }
