2a529024dc50c91193e9c8f19a260a29c79d5d38
Frédéric NOËL <contact@fredericnoel.com>
2020-03-16 15:12:05 +0100

    Ajout propriété "nombre de roues" avec valeur par défaut.

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -2,7 +2,7 @@
 
 class Voiture extends Vehicule
 {
-
+    protected $nombreRoues = 4;
 
     // Destructeur appelé automatiquement à la fin de l'exécution du script
     public function __destruct()
