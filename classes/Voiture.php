134080a8439b7c8f6dfdeb5757dca8728ae1c882
Frédéric NOËL <contact@fredericnoel.com>
2020-03-16 14:30:51 +0100

    Suppression du constructeur de la classe Voiture pour le positionner dans la classe parente Vehicule

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -2,17 +2,6 @@
 
 class Voiture extends Vehicule
 {
-    // On définit le constructeur
-    public function __construct(string $mar, string $mod, string $cou, int $mas)
-    {
-        // On affecte les paramètres passés à l'objet que l'on va instancier
-        // La variable $this désigne l'objet
-        $this->marque = $mar;
-        $this->modele = $mod;
-        $this->couleur = $cou;
-        $this->masse = $mas;
-    }
-
     // Getter pour la masse
     public function getMasse()
     {
