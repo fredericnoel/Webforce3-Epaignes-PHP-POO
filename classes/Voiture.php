49c4f289c1059f13326e3a690a6625bf1fd05585
Frédéric NOËL <contact@fredericnoel.com>
2020-03-16 14:55:36 +0100

    Suppression de méthode pour récupérer les informations des propriétés de l'objet. Héritage des getters/setters définis dans la classe mère

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -2,27 +2,7 @@
 
 class Voiture extends Vehicule
 {
-    // Getter pour la masse
-    public function getMasse()
-    {
-        return $this->masse;
-    }
 
-    public function setMasse(float $nouvelleMasse)
-    {
-        $this->masse = $nouvelleMasse;
-    }
-
-    // Méthode pour changer la couleur
-    public function changerCouleur(string $nouvelleCouleur) : void
-    {
-        $this->couleur = $nouvelleCouleur;
-    }
-
-    public function changerVitesse(float $variationVitesse)
-    {
-        $this->vitesse += $variationVitesse;
-    }
 
     // Destructeur appelé automatiquement à la fin de l'exécution du script
     public function __destruct()
