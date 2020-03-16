69c6d75a5de29d8f9c2bb7d8a11bd38e39c151c6
Frédéric NOËL <contact@fredericnoel.com>
2020-03-16 15:23:57 +0100

    Classe Formule 1

---
+++ classes/Formule1.php
@@ -0,0 +1,16 @@
+<?php
+
+class Formule1 extends Voiture
+{
+    private $caracteristique = 'monoplace';
+
+    public function getCaracteristique(): string
+    {
+        return $this->caracteristique;
+    }
+
+    public function setCaracteristique(string $caracteristique): void
+    {
+        $this->caracteristique = $caracteristique;
+    }
+}
